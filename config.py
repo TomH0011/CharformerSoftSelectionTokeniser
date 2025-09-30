6f5b697a5c08ebfca8d2b2043c266e04a58c8569
Tom Howard <165574283+TomH0011@users.noreply.github.com>
2025-09-30 01:31:33 +0100

    spelling error

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ embedding_dim = 8
 block_size = 3
 num_candidates = 3   # spans {2,3,4}
 stride = 2           # For Downsampling
-text = "charform"  # Text to be tokenised
+text = "charformer"  # Text to be tokenised
