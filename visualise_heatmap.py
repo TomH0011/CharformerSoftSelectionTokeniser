602b1e0283a771dab0848aa797d02b9cb44a04ed
Tom Howard <165574283+TomH0011@users.noreply.github.com>
2025-10-22 00:50:45 +0100

    fixed spelling error

--- visualise_heatmap.py
+++ visualise_heatmap.py
@@ -218,7 +218,7 @@ if __name__ == "__main__":
     visualiser = GBSTVisualiser()
 
     print("=" * 60)
-    print("GBST CHARFORMER BLOCK IMPORTANCE VISUALIsATION")
+    print("GBST CHARFORMER BLOCK IMPORTANCE VISUALISATION")
     print("=" * 60)
 
     # Create main heatmap
